deb2d8d5fe0010cbb03a179007129396ee512016
khyw407 <khyw407@gmail.com>
2021-10-27 16:09:21 +0900

    docs: 컴포넌트 스캔 옵션 테스트 코드 추가

--- spring-core/core/src/main/java/hello/core/AutoAppConfig.java
+++ spring-core/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,12 +1,22 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //예제코드 수정을 줄이기 위해 추가한 필터
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
