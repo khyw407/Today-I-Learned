41c84b96c4e807a7fe9cbf16f737643e428d6021
khyw407 <khyw407@gmail.com>
2021-10-27 15:18:49 +0900

    docs: component scan 코드 추가

---
+++ spring-core/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //예제코드 수정을 줄이기 위해 추가한 필터
+)
+public class AutoAppConfig {
+}
